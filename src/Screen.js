393d9881616091fbfeffc965b49042b6232f79bb
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-01-12 00:14:55 +0900

    Update Screen.js

--- src/Screen.js
+++ src/Screen.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { useSetLang, useT } from "./context";
+import { useSetLang, useSetT } from "./context";
 
 const Screen = () => {
     const setLang = useSetLang();
-    const t = useT();
+    const t = useSetT();
     return(
         <>
         <h1>{t("안녕")}</h1>
         <h1>Hello</h1>
         <button onClick ={()=>setLang("de")}>{t("번역")}</button>
-        </>
+        </> 
     )
 } 
 export default Screen;
